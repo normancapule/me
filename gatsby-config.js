36eba8c85bc04b92b09573114bbeb396593df9ce
John Norman Capule <johnnormancapule@gmail.com>
2020-06-04 00:10:21 +0800

    Add proper favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Normz Website`,
+        short_name: `normz`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1A202C`,
+        theme_color: `#1A202C`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
