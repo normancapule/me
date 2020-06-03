4afbcaa60dacc953fc9f0719ccee4e351eb28b3a
John Norman Capule <johnnormancapule@gmail.com>
2020-06-04 00:43:49 +0800

    Proper component naming

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import SEO from '../components/seo'
+import SEO from '../components/SEO'
 
 const NotFoundPage = (): JSX.Element => (
   <Layout>
